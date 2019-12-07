116ca9ffe53ed921a01b3cd15324af7dccab6568
jscmla1118 <jscmla1118@gmail.com>
2019-12-08 03:29:49 +0900

    Enable to control lights from nanoKONTROL2

--- Assets/VRChatLab/Scripts/LightController.cs
+++ Assets/VRChatLab/Scripts/LightController.cs
@@ -8,17 +8,19 @@ using System;
 public class LightController : MonoBehaviour
 {
     List<GameObject> lightShaftObjs = new List<GameObject>();
+    List<Color> baseColors = new List<Color>();
     List<Fader> faders = new List<Fader>();
     string midiControllerName;
+    int faderNum;
 
     // Start is called before the first frame update
     void Start()
     {
-        int faderNum = 8;
+        faderNum = 8;
         midiControllerName = "nanoKONTROL2";
 
         // Fader setting
-        for (int i=0; i < faderNum; i++)
+        for (int i = 0; i < faderNum; i++)
         {
             byte statusByte = Convert.ToByte("B0", 16);
             byte firstDataByte = Convert.ToByte("00", 16);
@@ -31,16 +33,23 @@ public class LightController : MonoBehaviour
         {
             GameObject obj = transform.GetChild(i).transform.Find("LightIcos").transform.Find("LightShaft45").gameObject;
             lightShaftObjs.Add(obj);
+            baseColors.Add(obj.GetComponent<Renderer>().material.color);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        foreach (GameObject obj in lightShaftObjs)
+        //for (int i = 0; i < faderNum; i++)
+        //{
+        //    Debug.Log(MidiMaster.GetKnob(MidiChannel.Ch1, i));
+        //}
+        //foreach (GameObject obj in lightShaftObjs)
+        for (int i=0; i<lightShaftObjs.Count; i++)
         {
-            Debug.Log(obj.name);
-            obj.GetComponent<Renderer>().material.color *= new Color(0.9f, 0.9f, 0.9f);
+            float w = MidiMaster.GetKnob(MidiChannel.Ch1, 0);
+            //Debug.Log(obj.name);
+            lightShaftObjs[i].GetComponent<Renderer>().material.color = baseColors[i] * w;
         }
     }
 }
