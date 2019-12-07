ac4ee7b95374066d1555b7f4fb9a29a6c52cabfd
jscmla1118 <jscmla1118@gmail.com>
2019-12-08 03:04:06 +0900

    Add FaderController class

---
+++ Assets/VRChatLab/Scripts/FaderController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace FaderController
+{
+    public class Fader
+    {
+        byte statusByte;
+        byte[] dataByte;
+
+        public Fader(byte statusByte, byte firstDataByte, byte secondDataByte)
+        {
+            this.statusByte = statusByte;
+            this.dataByte = new byte[2];
+            this.dataByte[0] = firstDataByte;
+            this.dataByte[1] = secondDataByte;
+        }
+    }
+}
