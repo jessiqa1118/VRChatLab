621aea9308434aedf9e1b331d357ee232342ec67
jscmla1118 <jscmla1118@gmail.com>
2019-12-08 03:07:33 +0900

    Add fader setting on start

--- Assets/VRChatLab/Scripts/LightController.cs
+++ Assets/VRChatLab/Scripts/LightController.cs
@@ -1,13 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MidiJack;
+using FaderController;
+using System;
 
 public class LightController : MonoBehaviour
 {
     List<GameObject> lightShaftObjs = new List<GameObject>();
+    List<Fader> faders = new List<Fader>();
+    string midiControllerName;
+
     // Start is called before the first frame update
     void Start()
     {
+        int faderNum = 8;
+        midiControllerName = "nanoKONTROL2";
+
+        // Fader setting
+        for (int i=0; i < faderNum; i++)
+        {
+            byte statusByte = Convert.ToByte("B0", 16);
+            byte firstDataByte = Convert.ToByte("00", 16);
+            byte secondDataByte = Convert.ToByte(i);
+            faders.Add(new Fader(statusByte, firstDataByte, secondDataByte));
+        }
+
+        // Object setting
         for (int i = 0; i < transform.childCount; i++)
         {
             GameObject obj = transform.GetChild(i).transform.Find("LightIcos").transform.Find("LightShaft45").gameObject;
