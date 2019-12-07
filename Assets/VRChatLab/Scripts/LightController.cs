ed7e01167f818f81b4e8d7e16eb455e0dd01686e
jscmla1118 <jscmla1118@gmail.com>
2019-12-08 02:18:33 +0900

    Add LightController

---
+++ Assets/VRChatLab/Scripts/LightController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    List<GameObject> lightShaftObjs = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            GameObject obj = transform.GetChild(i).transform.Find("LightIcos").transform.Find("LightShaft45").gameObject;
+            lightShaftObjs.Add(obj);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (GameObject obj in lightShaftObjs)
+        {
+            Debug.Log(obj.name);
+            obj.GetComponent<Renderer>().material.color *= new Color(0.9f, 0.9f, 0.9f);
+        }
+    }
+}
